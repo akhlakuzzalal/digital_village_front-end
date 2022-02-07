584ab38b3e70abf4f33bd1f1f9bc738b9392ed02
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-02-07 17:17:33 +0600

    Banner page create and new branch git push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <h1>Smart Village</h1>
+      <Banner/>
     </div>
   );
 }
