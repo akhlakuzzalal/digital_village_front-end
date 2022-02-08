f7a4361082ca45294ed309e3368aa5f56964f0a5
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-02-08 17:17:25 +0600

    deployed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/user/allusers')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <div className="App">
       <h1>Digital Village start here</h1>
+      {users.map((user) => (
+        <h2>{user.name}</h2>
+      ))}
     </div>
   );
 }
