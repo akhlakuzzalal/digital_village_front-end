469a323c9edecacdf9cae70d7b79f69db0ad7993
Safiaahmed6043 <safiaahmed1786@gmail.com>
2022-02-11 02:49:11 +0600

    [ADD] Review section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Home from './Pages/shared/Home/Home';
+import './App.css'
+import Home from './Pages/shared/Home/Home'
 
 function App() {
   return (
     <div>
       <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Pages/shared/Home/Home.jsx
+++ src/Pages/shared/Home/Home.jsx
@@ -3,6 +3,7 @@ import Footer from '../Home/Footer/Footer';
 import Header from '../Home/Header/Header';
 import LatestNews from '../Home/LatestNews/LatestNews';
 import Banner from './Banner/Banner';
+import Reviews from './Reviews/Reviews';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header />
       <Banner />
       <LatestNews />
+      <Reviews />
       <Footer />
     </div>
   );
