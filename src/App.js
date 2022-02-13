9b7b9a0dfc0341aa73ad23c4b182d37b6f8b8767
Saqline Shabbir <saqline5869@gmail.com>
2022-02-14 01:31:02 +0600

    changes in register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/shared/Home/Home.jsx';
 import './App.css';
+import Login from './pages/shared/Authentication/Login/Login.jsx';
 import Register from './pages/shared/Authentication/Register/Register.jsx';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Pages/shared/Authentication/Register/Register.jsx
+++ src/Pages/shared/Authentication/Register/Register.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { BsGoogle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import img from '../../../../assets/banner.png';
+import Header from '../../Home/Header/Header';
 const Register = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
     <>
+      <Header></Header>
       <div className="">
         <div className="flex">
           <div className="w-1/2 bg-white">
-            <div>
-              <h1>Creat an Accound</h1>
+            <div className="mt-32 mx-36 text-center">
+              <h3 className="">Create an Account</h3>
               <p className="my-3">
-                Already Registered? <Link to="/login">Log In</Link>
+                Already Registered?{' '}
+                <Link to="/login">
+                  <span className="text-primary">Log In</span>{' '}
+                </Link>
               </p>
+              <button className="flex bg-primary rounded-lg py-2 px-10 w-96 items-center lg:ml-20 ">
+                <BsGoogle
+                  className="h-8 w-8 hover:scale-110 hover:text-primary mx-3"
+                  aria-hidden="true"
+                />{' '}
+                <p className="">Sign Up With Google</p>
+              </button>
+
+              <p>or</p>
             </div>
-            <form
-              className="border-2 mx-32 my-80"
-              onSubmit={handleSubmit(onSubmit)}
-            >
+
+            <form className="lg:ml-52 " onSubmit={handleSubmit(onSubmit)}>
               <input
                 className="px-7 py-3 mx-3 my-3 bg-gray-100 w-48"
                 {...register('firstName', { required: true, maxLength: 20 })}
@@ -43,7 +56,7 @@ const Register = () => {
               />
 
               <input
-                className="bg-primary px-20 py-3"
+                className="bg-primary px-20 py-3 rounded-lg lg:ml-10"
                 type="submit"
                 value="Signup with Email"
               />
