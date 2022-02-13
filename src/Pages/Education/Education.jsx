0dd1554f86e254e73c8d7ee06efdd88793874103
coder71 <kamrunnaher877@gmail.com>
2022-02-13 22:49:20 +0600

    [ADD] different pages

---
+++ src/Pages/Education/Education.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Education = () => {
+  return <div>Here we will decide a user is teacher or student?</div>;
+};
+
+export default Education;
--- src/Pages/shared/Home/Service/Service.jsx
+++ src/Pages/shared/Home/Service/Service.jsx
@@ -1,117 +1,212 @@
 import React, { useState } from 'react';
-import ServiceBoard from './ServiceBoard';
-import learn from './../../../../assets/learn.png';
-import appointment from './../../../../assets/healthcare.png';
-import pepole from './../../../../assets/learn2.png';
 import donate from './../../../../assets/donate.png';
 import event from './../../../../assets/event.png';
+import appointment from './../../../../assets/healthcare.png';
+import learn from './../../../../assets/learn.png';
+import pepole from './../../../../assets/learn2.png';
+import ServiceBoard from './ServiceBoard';
 const Service = () => {
+  const [control, setControl] = useState('learn');
+  return (
+    <section className="space-y-6 md:space-y-0 flex flex-wrap py-8">
+      <ServiceBoard setControl={setControl} control={control}></ServiceBoard>
 
-    const [control, setControl] = useState("learn");
-    return (
-        <>
-    <section>
-    <div className="space-y-6 md:space-y-0 flex flex-wrap py-8">
-        <ServiceBoard setControl={setControl} control={control}></ServiceBoard>
-        {/* left banner svg */}
-        {control === "learn" && 
+      {/* Digital Learning */}
+      {control === 'learn' && (
         <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
-            <div className="flex items-center justify-center">
-            <img src= {learn} alt="village" className="w-full md:w-1/2 " />
-            </div>
-          <p className="text-ellipsis">Our project sector is e-governance. It will be a great platform to connect villagers with the village administration.</p>
-            <div class="flex items-center justify-center">
-
-            <a href="/" class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-                <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
+          <div className="flex items-center justify-center">
+            <img src={learn} alt="village" className="w-full md:w-1/2 " />
+          </div>
+          <p className="text-ellipsis">
+            Our project sector is e-governance. It will be a great platform to
+            connect villagers with the village administration.
+          </p>
+          <div class="flex items-center justify-center">
+            <a
+              href="/"
+              class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+            >
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="mr-2"
+                aria-hidden="true"
+              >
                 <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-                </svg>
-                Digital Learning
+              </svg>
+              Digital Learning
             </a>
-            </div>
-        </div>}
-        {control === "appoinment" && 
-        <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
-          <div className="flex items-center justify-center"><img src={appointment} alt="village" className="w-full md:w-1/2" />
           </div>
-          
-          <p className="text-ellipsis overflow-hidden">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id cupiditate fuga distinctio quia necessitatibus repudiandae! Iste illum quaerat eos, aliquam eligendi sit asperiores dolorem odit, iure, sed molestiae accusantium?</p>
-          <div class="flex items-center justify-center">
+        </div>
+      )}
 
-<a href="/" class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-    <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-    <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-    </svg>
-    Digital Learning
-</a>
-</div>
-        </div>}
-        {control === "pepole" && 
+      {/* Healthcare */}
+      {control === 'appoinment' && (
         <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
-          <div className="flex items-center justify-center"><img src={pepole} alt="village" className="w-full md:w-1/2" />
+          <div className="flex items-center justify-center">
+            <img src={appointment} alt="village" className="w-full md:w-1/2" />
           </div>
-          <p className="text-ellipsis overflow-hidden">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id cupiditate fuga distinctio quia necessitatibus repudiandae! Iste illum quaerat eos, aliquam eligendi sit asperiores dolorem odit, iure, sed molestiae accusantium?</p>
-          <div class="flex items-center justify-center">
 
-<a href="/" class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-    <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-    <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-    </svg>
-    Digital Learning
-</a>
-</div>
-        </div>}
-        {control === "donate" && 
+          <p className="text-ellipsis overflow-hidden">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id
+            cupiditate fuga distinctio quia necessitatibus repudiandae! Iste
+            illum quaerat eos, aliquam eligendi sit asperiores dolorem odit,
+            iure, sed molestiae accusantium?
+          </p>
+          <div class="flex items-center justify-center">
+            <a
+              href="/"
+              class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+            >
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="mr-2"
+                aria-hidden="true"
+              >
+                <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+              </svg>
+              Digital Learning
+            </a>
+          </div>
+        </div>
+      )}
+
+      {/* Development proposal */}
+      {control === 'pepole' && (
         <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
-
-          <div className="flex items-center justify-center"><img src={donate} alt="village" className="w-full md:w-1/2" />
+          <div className="flex items-center justify-center">
+            <img src={pepole} alt="village" className="w-full md:w-1/2" />
           </div>
-          <p className="text-ellipsis overflow-hidden">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id cupiditate fuga distinctio quia necessitatibus repudiandae! Iste illum quaerat eos, aliquam eligendi sit asperiores dolorem odit, iure, sed molestiae accusantium?</p>
+          <p className="text-ellipsis overflow-hidden">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id
+            cupiditate fuga distinctio quia necessitatibus repudiandae! Iste
+            illum quaerat eos, aliquam eligendi sit asperiores dolorem odit,
+            iure, sed molestiae accusantium?
+          </p>
           <div class="flex items-center justify-center">
+            <a
+              href="/"
+              class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+            >
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="mr-2"
+                aria-hidden="true"
+              >
+                <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+              </svg>
+              Digital Learning
+            </a>
+          </div>
+        </div>
+      )}
 
-<a href="/" class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-    <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-    <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-    </svg>
-    Digital Learning
-</a>
-</div>
-        </div>}
-        {control === "market" && 
+      {/* donation */}
+      {control === 'donate' && (
         <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
-          <div className="flex items-center justify-center"><img src="https://weirdguru.com/wp-content/uploads/2020/12/custom-ecommerce-website-development-vector-image.png" alt="village" className="w-full md:w-1/2"/>
+          <div className="flex items-center justify-center">
+            <img src={donate} alt="village" className="w-full md:w-1/2" />
           </div>
-          <p className="text-ellipsis overflow-hidden">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id cupiditate fuga distinctio quia necessitatibus repudiandae! Iste illum quaerat eos, aliquam eligendi sit asperiores dolorem odit, iure, sed molestiae accusantium?</p>
+          <p className="text-ellipsis overflow-hidden">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id
+            cupiditate fuga distinctio quia necessitatibus repudiandae! Iste
+            illum quaerat eos, aliquam eligendi sit asperiores dolorem odit,
+            iure, sed molestiae accusantium?
+          </p>
           <div class="flex items-center justify-center">
+            <a
+              href="/"
+              class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+            >
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="mr-2"
+                aria-hidden="true"
+              >
+                <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+              </svg>
+              Digital Learning
+            </a>
+          </div>
+        </div>
+      )}
 
-<a href="/" class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-    <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-    <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-    </svg>
-    Digital Learning
-</a>
-</div>
-        </div>}
-        {control === "event" && 
+      {/* e market */}
+      {control === 'market' && (
         <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
-          <div className="flex items-center justify-center"><img src={event} alt="village" className="w-full md:w-1/2" />
+          <div className="flex items-center justify-center">
+            <img
+              src="https://weirdguru.com/wp-content/uploads/2020/12/custom-ecommerce-website-development-vector-image.png"
+              alt="village"
+              className="w-full md:w-1/2"
+            />
           </div>
-          <p className="text-ellipsis overflow-hidden">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id cupiditate fuga distinctio quia necessitatibus repudiandae! Iste illum quaerat eos, aliquam eligendi sit asperiores dolorem odit, iure, sed molestiae accusantium?</p>
+          <p className="text-ellipsis overflow-hidden">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id
+            cupiditate fuga distinctio quia necessitatibus repudiandae! Iste
+            illum quaerat eos, aliquam eligendi sit asperiores dolorem odit,
+            iure, sed molestiae accusantium?
+          </p>
           <div class="flex items-center justify-center">
+            <a
+              href="/"
+              class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+            >
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="mr-2"
+                aria-hidden="true"
+              >
+                <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+              </svg>
+              Digital Learning
+            </a>
+          </div>
+        </div>
+      )}
 
-<a href="/" class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-    <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-    <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-    </svg>
-    Digital Learning
-</a>
-</div>
-        </div>}
-
-      </div>
+      {/* events */}
+      {control === 'event' && (
+        <div className="w-full md:w-1/2 place-self-center space-y-3 md:space-y-6 text-center md:text-center md:pl-24">
+          <div className="flex items-center justify-center">
+            <img src={event} alt="village" className="w-full md:w-1/2" />
+          </div>
+          <p className="text-ellipsis overflow-hidden">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id
+            cupiditate fuga distinctio quia necessitatibus repudiandae! Iste
+            illum quaerat eos, aliquam eligendi sit asperiores dolorem odit,
+            iure, sed molestiae accusantium?
+          </p>
+          <div class="flex items-center justify-center">
+            <a
+              href="/"
+              class="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+            >
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="mr-2"
+                aria-hidden="true"
+              >
+                <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+              </svg>
+              Digital Learning
+            </a>
+          </div>
+        </div>
+      )}
     </section>
-        </>
-    );
+  );
 };
 
 export default Service;
