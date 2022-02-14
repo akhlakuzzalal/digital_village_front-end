47cd0779ac2ef7c92743df88f61f4f20e627f65f
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-02-15 00:15:07 +0600

    [ADD] user profile

--- src/Pages/shared/Home/Home.jsx
+++ src/Pages/shared/Home/Home.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import Footer from '../Home/Footer/Footer';
+import Profile from '../../User/Profile';
 import Header from '../Home/Header/Header';
-import LatestNews from '../Home/LatestNews/LatestNews';
-import Banner from './Banner/Banner';
-import Reviews from './Reviews/Reviews';
-import Service from './Service/Service';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Banner />
-      <Service />
-      <LatestNews />
-      <Reviews />
-      <Footer />
+      <Profile></Profile>
     </div>
   );
 };
