e3cc78dc6dd596dbbf8415c20dcf4ff865148857
Saqline Shabbir <saqline5869@gmail.com>
2022-02-14 12:09:53 +0600

    Changes in Login signup

--- src/Pages/shared/Authentication/Register/Register.jsx
+++ src/Pages/shared/Authentication/Register/Register.jsx
@@ -11,9 +11,9 @@ const Register = () => {
     <>
       <Header></Header>
       <div className="">
-        <div className="flex">
-          <div className="w-1/2 bg-white">
-            <div className="mt-32 mx-36 text-center">
+        <div className="lg:flex md:flex">
+          <div className="lg:w-1/2 md:w-1/2 sm:w-auto bg-white">
+            <div className="pt-48 mx-10 text-center lg:mx-48 ">
               <h3 className="">Create an Account</h3>
               <p className="my-3">
                 Already Registered?{' '}
@@ -21,48 +21,51 @@ const Register = () => {
                   <span className="text-primary">Log In</span>{' '}
                 </Link>
               </p>
-              <button className="flex bg-primary rounded-lg py-2 px-10 w-96 items-center lg:ml-20 ">
+              <button className="flex bg-primary rounded-lg py-2 px-10 lg:w-full md:w-96 items-center w-full mx-auto">
                 <BsGoogle
-                  className="h-8 w-8 hover:scale-110 hover:text-primary mx-3"
+                  className="h-8 w-8 hover:scale-110 hover:text-secondary mx-3"
                   aria-hidden="true"
-                />{' '}
-                <p className="">Sign Up With Google</p>
+                />
+                <p className="sm:text-center ">Sign Up With Google</p>
               </button>
 
               <p>or</p>
             </div>
 
-            <form className="lg:ml-52 " onSubmit={handleSubmit(onSubmit)}>
-              <input
-                className="px-7 py-3 mx-3 my-3 bg-gray-100 w-48"
-                {...register('firstName', { required: true, maxLength: 20 })}
-                placeholder="First Name"
-              />
-              <input
-                className="px-7 py-3 mx-3 my-3 bg-gray-100 w-48"
-                {...register('lastName', { pattern: /^[A-Za-z]+$/i })}
-                placeholder="Last Name"
-              />
+            <form className=" lg:mx-48 mx-10" onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex">
+                <input
+                  className="px-7 py-3  my-3 bg-gray-100 w-full"
+                  {...register('firstName', { required: true, maxLength: 20 })}
+                  placeholder="First Name"
+                />
+                <input
+                  className="px-7 py-3 ml-3 my-3 bg-gray-100 w-full"
+                  {...register('lastName', { pattern: /^[A-Za-z]+$/i })}
+                  placeholder="Last Name"
+                />
+              </div>
 
               <input
-                className="px-7 py-3 mx-3 my-3 w-96 bg-gray-100"
+                className="px-7 py-3  my-3  bg-gray-100 w-full"
                 {...register('email')}
                 placeholder="Email"
               />
               <input
-                className="px-7 py-3 mx-3 my-3 w-96 bg-gray-100"
-                {...register('email')}
-                placeholder="DropDown"
+                type="password"
+                className="px-7 py-3 my-3  bg-gray-100 w-full"
+                {...register('password')}
+                placeholder="Password"
               />
 
               <input
-                className="bg-primary px-20 py-3 rounded-lg lg:ml-10ggit "
+                className="bg-primary px-20 py-3 rounded-lg  sm:mb-20 w-full mx-auto mb-20"
                 type="submit"
                 value="Signup with Email"
               />
             </form>
           </div>
-          <div className="w-1/2">
+          <div className="w-1/2 hidden md:block">
             <img src={img} alt="" />
           </div>
         </div>
