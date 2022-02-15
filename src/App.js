c352512c1a928300ea5a9f828dffcc5ee2f11274
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-02-15 13:09:26 +0600

    about-us page create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import AboutUs from './Components/About-Us/AboutUs';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Digital Village start here</h1>
+    <div>
+      {/* <h1>Digital Village start here</h1>
+      <Banner /> */}
+      <AboutUs/>
     </div>
   );
 }
