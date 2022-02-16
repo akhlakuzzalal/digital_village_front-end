5b22c576f5eab12b0f942074cd7b4f88f2eaf0c3
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-02-17 01:46:10 +0600

    Contact us page Design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import AboutUs from './Components/About-Us/AboutUs';
 import Banner from './Components/Banner/Banner';
+import ContactUs from './Components/Contact-Us/ContactUs';
 
 function App() {
   return (
     <div>
       {/* <h1>Digital Village start here</h1>
       <Banner /> */}
-      <AboutUs/>
+      {/* <AboutUs/> */}
+      <ContactUs/>
     </div>
   );
 }
---
+++ src/Components/Contact-Us/ContactUs.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+
+
+
+const ContactUs = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+     
+  
+    return (
+        <div>
+            <div>
+                <h1 class="text-5xl text-center mt-12 font-bold">Contact Us</h1>
+                <h3 class="text-center font-semibold my-3">Home  / Contact Us</h3>
+            </div>
+            <div class="pr-8">
+                <h3 class="text-center text-2xl font-semibold mt-16 mb-3 text-gray-500">FOLLOW OUR INFO</h3>
+                <h1 class="text-5xl text-center font-bold">Contact information</h1>
+                <h4 class="text-center mt-5  mx-10 container text-1xl">Give us a call or drop by anytime, we endeavour to answer all enquiries within 24 hours on business days.We will be happy to answer your questions.enquiries within 24 hours on business days.We will be happy to answer your questions.</h4>
+            </div>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-10 Container">
+                {/* <div > 10:00am to 6:00pm Sunday Closed </div>
+                <div > 13005 Greenville Avenue California, TX 70240 </div>
+                <div > +1 000999 0099 info@themerange.net </div>
+                <div >
+                     <h3 class="text-3xl font-semi-bold">Our Socials Links</h3>
+                     <p>Print this page to PDF for the complete set of vectors.</p>
+                 </div> */}
+                <div  class="flex flex-col items-center bg-white rounded-lg pl-10  md:flex-row pl-15     container md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <div class="bg-gray-100 rounded-full">
+                    <img class="object-cover rounded-full h-20 w-20" src="https://freepngimg.com/thumb/clock/58348-alarm-icon-cartoon-timer-clock-free-photo-png-thumb.png" alt=""/>
+                    </div>
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <p class="mb-3 font-semibold text-gray-700 dark:text-gray-400">10:00am to 6:00pm</p>
+                    <p class="mb-3 font-semibold text-gray-700 dark:text-gray-400">Sunday Closed</p>
+                   </div>
+                </div>
+
+                <div  class="flex flex-col items-center bg-white rounded-lg pl-5 md:flex-row  ml-15    container md:max-w-xl hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <img class="object-cover rounded-full h-20 w-20  " src="https://image.pngaaa.com/23/203023-middle.png" alt=""/>
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <p class="mb-3 font-semibold text-gray-700 dark:text-gray-400">13005 Greenville Avenue</p>
+                    <p class="mb-3 font-semibold text-gray-700 dark:text-gray-400">California, TX 70240</p>
+                   </div>
+                </div>
+
+                <div  class="flex flex-col items-center bg-white rounded-lg  md:flex-row pl-10  container md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <img class="object-cover rounded-full h-20 w-20" src="https://www.kindpng.com/picc/m/336-3369375_contact-flower-mound-electrician-today-phone-icon-png.png" alt=""/>
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <p class="mb-3  text-gray-700 dark:text-gray-400 font-semibold">+1 000999 0099</p>
+                    <p class="mb-3  text-gray-700 dark:text-gray-400 font-semibold">info@themerange.net</p>
+                   </div>
+                </div>
+
+                <div  class="flex flex-col items-center bg-white rounded-lg  md:flex-row      container md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h3 class="mb-3  text-gray-700 dark:text-gray-400 text-3xl font-bold">Our Socials Links</h3>
+                    <p class="mb-3 font-semibold text-gray-700 dark:text-gray-400">Print this page to PDF for the complete set of vectors.</p>
+                    <div class="inline-grid grid-cols-6">
+                       <img class=" d-flex object-cover rounded-full h-10 w-10 m-2" src="https://pnggrid.com/wp-content/uploads/2021/05/Facebook-logo-2021.png " alt=""/>
+                       <img class=" d-flex object-cover rounded-full h-10 w-10 m-2" src="https://p1.hiclipart.com/preview/1016/686/474/mitu-icon-twitter-png-clipart.jpg" alt=""/>
+                       <img class="object-cover rounded-full h-10 w-10 m-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png" alt=""/>
+                       <img class="object-cover rounded-full h-10 w-10 m-2" src="https://i.pinimg.com/originals/72/9f/77/729f7798561be2cb67f39e916a22eb6a.png" alt=""/>
+                   </div>
+                   </div>
+                   
+                </div>
+
+                
+
+            </div>
+            <div class="mt-10">
+                <h5 class="text-center text-1xl font-bold mt-16 mb-3 text-gray-600">DON’T HASITATE TO CONTACT WITH US</h5>
+                <h1 class="text-5xl text-center font-bold">Now Very Easy</h1>
+                <h4 class="text-center mt-5  px-10 container text-1xl">Our approach to SEO is uniquely built around what we know works…and what we know doesn’t work. With over 200 verified factors in playworks…and what we know doesn’t work. With over 200 verified factors in play.</h4>
+            </div>
+
+            <div class=" w-full bg-black">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input class="w-3/6 " {...register("firstName", { required: true, maxLength: 20 })} />
+                    <input class="w-3/6 " {...register("firstName", { required: true, maxLength: 20 })} />
+                    <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                    <input type="number" {...register("age", { min: 18, max: 99 })} />
+                    <input type="submit" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default ContactUs;
