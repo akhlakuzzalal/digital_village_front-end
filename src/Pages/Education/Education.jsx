4f991bb080030e569fc63df0717f42c65dfef457
Safiaahmed6043 <safiaahmed1786@gmail.com>
2022-02-20 18:54:38 +0600

    [update]eduction & Reviews

--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -6,43 +6,40 @@ import study from '../../assets/education/study.png';
 
 const Education = () => {
   return (
-    <div
-      className="bg-gradient-to-r mt-[88px]"
-      style={{ minHeight: 'calc(100vh - 700px)' }}
-    >
-      <h1 className="text-center">Digital Learning</h1>
-      <div className="flex justify-center items-center">
-        <div className="flex justify-center items-center w-1/2">
-          <Link to="/teacherinfo">
-            <div className=" border-2 bg-secondary hover:bg-slate-50  rounded-lg text-center py-9  md:py-16  border-primary mx-2  flex justify-center items-center  my-2 ">
-              <div className="bg-white border-2  border-primary rounded-full w-full md:w-1/2  my-2 flex justify-center items-center">
+    <div className="  py-5 mt-[88px]">
+      <h1 className="text-secondary text-center ">Digital Learning</h1>
+      <div className=" flex  flex-wrap justify-center items-center  ">
+        <div className="flex flex-wrap justify-center items-center  w-full md:w-1/2 ">
+          <Link to="#">
+            <div className=" border-2 bg-purple-200 hover:bg-slate-50  rounded-lg text-center py-9  md:py-16  border-slate-200 mx-2  flex justify-center items-center  my-2 ">
+              <div className="bg-white border-2 border-slate-200  rounded-full w-full md:w-1/2  my-2 flex justify-center items-center ">
                 <h1>
                   <GiTeacher />
                 </h1>
               </div>
 
-              <h3 className="Neutral-500 md:heading_sm px-0 md:px-8 capitalize">
-                i am a teacher
+              <h3 className="Neutral-500 md:heading_sm px-0 md:px-8">
+                Join As A Teacher
               </h3>
             </div>
           </Link>
-          <Link to="/studentinfo">
-            <div className=" border-2 border-secondary  bg-primary hover:bg-slate-50 rounded-lg text-center py-9  md:py-16   mx-2  flex justify-center items-center  my-2">
-              <div className="bg-white border-2 border-secondary rounded-full flex justify-center items-center w-full md:w-1/2  my-2  ">
+          <Link to="#">
+            <div className=" border-2 border-slate-200 bg-pink-200   hover:bg-slate-50 rounded-lg text-center py-9  md:py-16   mx-2  flex justify-center items-center  my-2">
+              <div className="bg-white border-2 border-slate-200 rounded-full flex justify-center items-center w-full md:w-1/2  my-2  ">
                 <h1>
                   <FaChalkboardTeacher />
                 </h1>
               </div>
 
               <button className="">
-                <h3 className="Neutral-500 md:heading_sm px-0 md:px-8 capitalize">
-                  I am a student
+                <h3 className="Neutral-500 md:heading_sm px-0 md:px-8">
+                  Join As A Student
                 </h3>
               </button>
             </div>
           </Link>
         </div>
-        <div className="flex justify-center items-center w-1/2">
+        <div className="w-full md:w-1/2 flex justify-center items-center">
           <img src={study} alt="" />
         </div>
       </div>
--- src/Pages/shared/Home/Reviews/Review/Review.jsx
+++ src/Pages/shared/Home/Reviews/Review/Review.jsx
@@ -1,37 +1,22 @@
 import React from 'react';
-//import icon
-import { AiFillStar } from 'react-icons/ai';
-//import image
-import client from '../../../../../assets/reviews/review.jpg';
+import client from './../../../../../assets/reviews/review.jpg';
+import Rating from './../../../../../components/Rating';
 
 const Review = () => {
+  const rate = { rating: 4 };
   return (
     <div className="w-3/4 static ">
       <div className="w-16 md:w-28  relative top-9 -right-10  md:top-44 md:right-11 ">
         <img
-          className="w-full rounded-full border-4 border-white"
+          className="w-full rounded-full border-4 border-slate-200"
           src={client}
           alt=""
         />
       </div>
-      <div className="bg-white  border-2 rounded-lg text-center py-9  md:py-16 px-0 md:px-8 border-secondary">
-        <ul className="flex justify-center text-yellow-400 heading_sm md:heading_md">
-          <li>
-            <AiFillStar />
-          </li>
-          <li>
-            <AiFillStar />
-          </li>
-          <li>
-            <AiFillStar />
-          </li>
-          <li>
-            <AiFillStar />
-          </li>
-          <li>
-            <AiFillStar />
-          </li>
-        </ul>
+      <div className="bg-white  border-4 rounded-lg text-center py-9  md:py-16 px-0 md:px-8 border-slate-50">
+        <div className="flex justify-center text-yellow-400 heading_sm md:heading_md">
+          <Rating rating={rate.rating} />
+        </div>
 
         <p className="Neutral-500 md:heading_sm px-0 md:px-8">
           Lorem ipsum dolor sit amet consectetur.
