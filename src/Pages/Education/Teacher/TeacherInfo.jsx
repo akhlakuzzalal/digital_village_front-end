1f2f9ddf35f087f9a785777d12b678cf72ebd1b7
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-02-21 11:43:21 +0600

    Vaccine Registation page some of Modified

--- src/Pages/Education/Teacher/TeacherInfo.jsx
+++ src/Pages/Education/Teacher/TeacherInfo.jsx
@@ -12,33 +12,37 @@ const TeacherInfo = () => {
   const navigate = useNavigate();
   return (
     <div className="mt-[88px]" style={{ minHeight: 'calc(100vh - 700px)' }}>
-      <form className=" space-y-6 mt-10 w-2/2 md:w-1/2 lg:w-1/2 mx-auto" onSubmit="">
+      <form className=" space-y-6 mt-10 w-2/2 md:w-1/2  mx-auto" onSubmit="">
         {/* email */}
         <input
           className="px-7 py-2 bg-gray-100 outline-none border-2 focus:border-primary w-full transition-all duration-300 rounded-lg"
-          {...register('title')}
+          {...register('title',{required: true})}
           placeholder="title"
+          required
         />
         <input
           className="px-7 py-10 bg-gray-100 outline-none border-2 focus:border-primary w-full transition-all duration-300 rounded-lg"
-          {...register('description')}
+          {...register('description',{required: true})}
           placeholder="description"
+          required
         />
         <input
           type="Date"
           className="px-7 py-2 bg-gray-100 outline-none border-2 focus:border-primary w-full transition-all duration-300 rounded-lg"
-          {...register('date')}
+          {...register('date',{required: true})}
           placeholder="title"
+          required
         />
         <input
           className="px-7 py-2 bg-gray-100 outline-none border-2 focus:border-primary w-full transition-all duration-300 rounded-lg"
-          {...register('title')}
+          {...register('title' ,{required: true})}
           placeholder="title"
         />
         <input
           className="px-7 py-2 bg-gray-100 outline-none border-2 focus:border-primary w-full transition-all duration-300 rounded-xl"
-          {...register('title')}
+          {...register('title' ,{required: true})}
           placeholder="title"
+          required
         />
 
         {/* submit button */}
