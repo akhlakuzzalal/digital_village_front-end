fdeb007f83385874566da0dfd00b6ef403834d1f
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-02-23 00:50:04 +0600

    [MOD] bug

--- src/Pages/Medical/Dashboard/partials/dashboardItem/VaccineRegistration/VaccineRegistration.jsx
+++ src/Pages/Medical/Dashboard/partials/dashboardItem/VaccineRegistration/VaccineRegistration.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import covax from './../../../../../../assets/medical/covax.jpg';
+import covax from './../../../../../../assets/medical/COVAX.jpg';
 
 const VaccineRegistration = () => {
   return (
