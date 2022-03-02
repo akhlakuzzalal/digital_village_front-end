7657b118f4737737dcc265d611951c58f41da2fd
Safiaahmed6043 <safiaahmed1786@gmail.com>
2022-03-03 04:32:09 +0600

    [Add]regform added

--- src/Pages/Medical/Dashboard/partials/dashboardItem/DoctorAppointment/UserAppointments/UserAppointment.jsx
+++ src/Pages/Medical/Dashboard/partials/dashboardItem/DoctorAppointment/UserAppointments/UserAppointment.jsx
@@ -13,7 +13,7 @@ const UserAppointment = ({ date }) => {
   console.log(appointment);
   return (
     <div class="flex flex-col">
-      <h4>your Total Appointment{appointment.length}</h4>
+      <h4> Total Appointments:{appointment.length}</h4>
       <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
           <div class="overflow-hidden shadow-md sm:rounded-lg">
