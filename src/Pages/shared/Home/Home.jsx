4c7affd9f19554a78c51b3bc8448143a95a2eed4
Saqline Shabbir <saqline5869@gmail.com>
2022-03-03 12:53:40 +0600

    Faq added and changes in event card

--- src/Pages/shared/Home/Home.jsx
+++ src/Pages/shared/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Analyticale from './Analyticale/Analyticale.js';
 import Banner from './Banner/Banner';
+import Faq from './Faq/Faq.jsx';
 import News from './News/News';
 import Reviews from './Reviews/Reviews';
 import Service from './Service/Service';
@@ -13,6 +14,7 @@ const Home = () => {
       <News />
       <Reviews />
       <Analyticale />
+      <Faq></Faq>
     </div>
   );
 };
