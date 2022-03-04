16d9b1b7b14be0e447d9dd601a69215573030103
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-03-04 19:11:51 +0600

    [MOD] user menu

--- src/Components/UserMenu.jsx
+++ src/Components/UserMenu.jsx
@@ -108,8 +108,7 @@ const UserMenu = () => {
               <li className="hover:bg-green-500">
                 <Link
                   className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3"
-                  to="/"
-                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                  to="/userdashboard"
                 >
                   User dashboard
                 </Link>
@@ -118,8 +117,7 @@ const UserMenu = () => {
               <li className="hover:bg-green-500">
                 <Link
                   className="font-medium text-sm text-indigo-500 hover:text-indigo-600 flex items-center py-1 px-3"
-                  to="/"
-                  onClick={() => setDropdownOpen(!dropdownOpen)}
+                  to="/admin"
                 >
                   Admin dashboard
                 </Link>
