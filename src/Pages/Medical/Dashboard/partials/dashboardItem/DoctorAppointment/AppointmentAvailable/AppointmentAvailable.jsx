4c128ced5f565552d9c97a55ec119751354f02b9
Safia Jotey <safiaahmed1786.com>
2022-03-10 11:47:07 -0800

    [check]bug

--- src/Pages/Medical/Dashboard/partials/dashboardItem/DoctorAppointment/AppointmentAvailable/AppointmentAvailable.jsx
+++ src/Pages/Medical/Dashboard/partials/dashboardItem/DoctorAppointment/AppointmentAvailable/AppointmentAvailable.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import swal from 'sweetalert';
+import Appointment from './Appointment';
 
 // import Appointment from './Appointment';
 
@@ -37,26 +38,9 @@ const AppointmentAvailable = ({ date }) => {
       </h3>
       <div className="flex flex-wrap justify-center items-center my-5 ">
         <div class="w-full md:w-1/4 m-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ccc hover:scale-110 transition-all  border-b-indigo-500 duration-500">
-          <div class="p-5">
-            <Link to="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight hover:text-blue-600 text-gray-900 dark:text-white">
-                Consultation
-              </h5>
-            </Link>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Any types of routine checkups and screening tests, give you flu
-              and immunization shots.
-            </p>
-
-            <p className="my-.5">(from 11am to 5pm ) </p>
-            <p>Cost : 10$ </p>
-            <Link
-              to="#"
-              class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-primary rounded-lg "
-            >
-              <button onClick={handleAlert}>Book Appointment</button>
-            </Link>
-          </div>
+          {appointment.map((app) => (
+            <Appointment app={app} />
+          ))}
         </div>
 
         <div class="w-full md:w-1/4 m-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ccc hover:scale-110 transition-all  border-b-indigo-500 duration-500">
