5a07a8903a6f9323800d03ad684f433325791f1d
coder71 <kamrunnaher877@gmail.com>
2022-03-16 12:53:23 +0600

    [BUG] student home page data fetching fixed

--- src/Pages/Education/Student/StudentHome/StudentHome.jsx
+++ src/Pages/Education/Student/StudentHome/StudentHome.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchBlogs,
+  setBlogCurrPage,
+} from '../../../../redux/slices/blog/blogSlice';
+import {
+  fetchVideos,
+  setVideoCurrPage,
+} from '../../../../redux/slices/video/videoSlice';
 import BlogCard from '../AllBlogs/BlogCard/BlogCard';
 import VideoCard from '../AllVideos/VideoCard/VideoCard';
 import Banner from './Banner/Banner';
@@ -8,6 +16,16 @@ import Categories from './Categories/Categories';
 const StudentHome = () => {
   const videos = useSelector((state) => state.videos.videos);
   const blogs = useSelector((state) => state.blogs.blogs);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setVideoCurrPage(0);
+    setBlogCurrPage(0);
+    dispatch(fetchBlogs({}));
+    dispatch(fetchVideos({}));
+    console.log('I am here');
+  }, []);
   return (
     <div className="space-y-6">
       <Banner />
@@ -20,14 +38,14 @@ const StudentHome = () => {
       {/* top blogs */}
       <h3>Top Blogs</h3>
       <div className="flex flex-wrap items-center gap-6">
-        {blogs.map((blog) => (
+        {blogs.slice(0, 6).map((blog) => (
           <BlogCard key={blog._id} blog={blog} />
         ))}
       </div>
 
       <h3 className="py-6">Top Videos</h3>
       <div className="flex flex-wrap items-center gap-6">
-        {videos.map((video) => (
+        {videos.slice(0, 6).map((video) => (
           <VideoCard key={video._id} video={video} />
         ))}
       </div>
