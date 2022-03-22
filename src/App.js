5fac9b4e60198bbee67753fb1b69dd77acfd43fc
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-03-23 00:02:13 +0600

    app js routes

--- src/App.js
+++ src/App.js
@@ -223,7 +223,7 @@ const App = () => (
 
           {/* 
           Admin dashboard routes */}
-          {/* <Route element={<AdminRoute allowedRoles={[Roles.Admin]} />}> */}
+          <Route element={<AdminRoute allowedRoles={[Roles.Admin]} />}>
             <Route path="admin" element={<AdminDashboard />}>
               <Route path="allusers" element={<AllUsers />} />
               <Route path="sendNotification" element={<SendNotification />} />
@@ -250,7 +250,7 @@ const App = () => (
               <Route path="status" element={<StatusCheck />} />
               <Route path="allHelpRequests" element={<AllHelpRequests />} />
             </Route>
-          {/* </Route> */}
+          </Route>
 
           {/* unauthorized route */}
           <Route path="unauthorized" element={<UnAuthorized />} />
