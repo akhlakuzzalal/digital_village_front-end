3d64f289caf58c98ef2bf0cd74138f5b3e9c245f
coder71 <kamrunnaher877@gmail.com>
2022-03-24 01:21:27 +0600

    [MOD] bannner

--- src/Pages/shared/Home/Banner/Banner.jsx
+++ src/Pages/shared/Home/Banner/Banner.jsx
@@ -32,13 +32,13 @@ const Banner = () => {
       >
         {/* banner description */}
         <div className="w-full mx-auto lg:mx-0 place-self-center mt-6 lg:mt-24 order-1 text-center lg:text-left lg:ml-80">
-          <h1 className='text-3xl md:text-5xl'>Digital Village</h1>
+          <h1 className="text-3xl md:text-5xl">Digital Village</h1>
           <p className="pb-3 w-5/6 md:w-4/6  mx-auto lg:mx-0 text-sm text-center md:text-left font-extralight mt-3 text-gray-600">
             We the village administration provide quality services through this
             platform. Every villagers can fit their needs by utilizing the
             facilities we provide.
           </p>
-          <div className="flex items-start justify-center md:justify-start">
+          <div className="w-fit mx-auto md:w-full md:mx-0">
             <NavHashLink smooth to="/#service">
               <button className="btn rounded-lg bg-primary dark:bg-dark_secondary">
                 Services
@@ -49,7 +49,7 @@ const Banner = () => {
         <div className="lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3 order-3 lg:order-2">
           <Features />
         </div>
-        {/* banner svg */}
+        {/* banner lottie */}
         <div className="w-full pointer-events-none  lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:col-end-3 order-2 lg:order-3">
           <div className="w-fit mx-auto">
             <Lottie
