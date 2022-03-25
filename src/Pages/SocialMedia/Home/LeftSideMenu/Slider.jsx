1a8f13341e8d6700e37c700b2b6a3d1d53199a81
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-03-25 12:41:40 +0600

    [MOD] social

--- src/Pages/SocialMedia/Home/LeftSideMenu/Slider.jsx
+++ src/Pages/SocialMedia/Home/LeftSideMenu/Slider.jsx
@@ -36,70 +36,18 @@ const Slider = () => {
           <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
             <img
               className="w-full h-40 "
-              src="https://krishokdotcom.net/wp-content/uploads/2019/12/ata1.jpg"
+              src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/08/top-5-reasons-why-you-need-a-social-media-manager-616015983b3ba-sej-760x400.png"
               alt=""
             />
-            <p>Flour</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
             <img
               className="w-full h-40 "
-              src="https://chaldn.com/_mpimage/fresh-fruits?src=https%3A%2F%2Feggyolk.chaldal.com%2Fapi%2FPicture%2FRaw%3FpictureId%3D61651&q=low&v=1&m=400"
+              src="https://api.backlinko.com/app/uploads/2020/08/social-media-users-post-banner.png"
               alt=""
             />
-            <p>Fruits</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
-            <img
-              className="w-full h-40 "
-              src="https://lh3.googleusercontent.com/skrw8TXUCGmYTw7e5pxj5z0VK7rMuSchKuOyRO5U7bsCnib-VcQm0ud7oET6QMmLh3BXhsX7j9W4w8lK-qCOotuGI1DJ7xCFZg=s1500-pp"
-              alt=""
-            />
-            <p>Oil</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
-            <img
-              className="w-full h-40 "
-              src="https://modonali.com/public/uploads/all/1lifcjBuZvrHG717B8r0dxOQYDAGDvKlPIcCtmVo.jpg"
-              alt=""
-            />
-            <p>Soap</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
-            <img
-              className="w-full h-40 "
-              src="https://cdn.britannica.com/17/176517-050-6F2B774A/Pile-uncooked-rice-grains-Oryza-sativa.jpg"
-              alt=""
-            />
-            <p>Rice</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
-            <img
-              className="w-full h-40 "
-              src="https://5.imimg.com/data5/IR/QZ/ST/ANDROID-90279950/product-jpeg-500x500.jpg"
-              alt=""
-            />
-            <p>Vegetable</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="rounded-lg w-fit flex flex-col justify-center items-center hover:scale-105 duration-300 dark:dark-card-bg">
-            <img
-              className="w-full h-40 "
-              src="https://www.spicejungle.com/wp/spice/files/2015/10/how-to-properly-store-spices.jpg"
-              alt=""
-            />
-            <p>Spices</p>
           </div>
         </SwiperSlide>
       </Swiper>
