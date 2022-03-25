d94cff591a4753580e8035df09d19d362659ff8e
Saqline Shabbir <saqline5869@gmail.com>
2022-03-25 11:53:58 +0600

    changes

--- src/Pages/shared/Home/Reviews/Review/Review.jsx
+++ src/Pages/shared/Home/Reviews/Review/Review.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import client from './../../../../../assets/reviews/review.jpg';
 import Rating from './../../../../../Components/Rating';
 
 const Review = ({ reviews }) => {
-  const { name, description, rating } = reviews;
+  const { name, description, rating, image } = reviews;
 
   return (
-    <div className="md:pb-12">
-      <div className="w-16 md:w-28 relative top-8 left-40 md:top-12 md:left-34 ">
+    <div className="md:pb-12 w-full">
+      <div className="w-full md:w-28 relative top-8 left-40 md:top-12 md:left-34 ">
         <img
           className="w-full rounded-full border-4 border-slate-200 dark:border-secondary"
-          src={client}
+          src={image}
           alt=""
         />
       </div>
-      <div className="bg-white dark:dark-card-bg dark:border-secondary shadow-md border-4 rounded-lg text-center py-9  md:py-16 px-8  border-slate-50 w-[400px] md:w-[420px] ">
+      <div className="bg-white dark:dark-card-bg dark:border-secondary shadow-md border-4 rounded-lg text-center py-9  md:py-16 px-8  border-slate-50 w-full md:w-[420px] ">
         <div className="flex justify-center text-yellow-400 heading_sm md:heading_md">
           <Rating rating={rating} />
         </div>
